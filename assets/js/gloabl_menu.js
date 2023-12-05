439c8a5cae4130b5ca7acf76fcaf1f95373ab9eb
Adelene <47531026+AdeleneLSW15@users.noreply.github.com>
2023-12-05 19:06:16 +0000

    commit initial files to repo

---
+++ assets/js/gloabl_menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Load and insert the global header, menu, and footer content
+    $("#menu-container").load("menu.html");  
+    $("#header-container").load("header.html");  
+    $("#footer-container").load("footer.html");  
+  });
